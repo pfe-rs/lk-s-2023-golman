36ff5dc3ff0641485e33902633d3a6c2c3860461
Despot Maksimovic <despotmaksimovic@gmail.com>
2023-07-19 16:19:05 +0200

    finalized dip.py

--- dip.py
+++ dip.py
@@ -1,79 +1,97 @@
 # import necessary libraries
 import cv2
 import numpy as np
-import colorsys
 
-
-def setupVidCap():
-    vidcap = cv2.VideoCapture(2)
+def setupVidCap(vidCapId: int = 2):
+    vidcap = cv2.VideoCapture(vidCapId)
     vidcap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920.0)
     vidcap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080.0)
     return vidcap
 
 
-def loadImage(vidcap):
+def loadImage(vidcap: cv2.VideoCapture):
     _, image = vidcap.read()
+    showImage(image)
     return image
 
-# vidcap.release()
 
-# cv2.imshow("", image)
-# cv2.waitKey(0)
+def closeVidCap(vidcap: cv2.VideoCapture):
+    vidcap.release()
 
-upperBound = colorsys.rgb_to_hsv(255,190,170) # hsv(14, 33%, 100%)
-lowerBound = colorsys.rgb_to_hsv(255,160,150) # hsv(6, 41%, 100%)
 
-hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-# mask = cv2.inRange(hsv_image, lowerBound, upperBound)
+def showImage(image, title: str = "Image", waitKeyTimeout:int = 0):
+    cv2.imshow(title,image)
+    cv2.waitKey(waitKeyTimeout)
 
-max_value = 255
-max_value_H = 360
-low_H = 0
-low_S = 79
-low_V = 255
-high_H = 9
-high_S = 90
-high_V = 255
 
-uglovi = 0
-konture = None
+def getDialatedEdges(vidcap: cv2.VideoCapture, image, kernel_size: tuple = (10,10), anchor_size: tuple = (4,4), lower_bound: tuple = (0,79,255), upper_bound: tuple = (9,90,255)):
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-while uglovi != 4:
-    try:
-        mask = cv2.inRange(hsv_image, (low_H, low_S, low_V), (high_H, high_S, high_V))
-        element = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10), (4, 4))
-        dilatation_dst = cv2.dilate(mask, element)
-        # cv2.imshow("Lol", dilatation_dst)
-        # cv2.waitKey(0)
-        # print(len(contours))
-        contours, _ = cv2.findContours(dilatation_dst, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        uglovi = len(contours)
-        print(f"treuntno: {uglovi}")
-        konture = contours
-    except KeyboardInterrupt:
-        break
+    uglovi = 0
+    konture = None
 
-c = max(contours, key=cv2.contourArea)
-print(f"konacno: {len(konture)}")
-x, y, w, h = cv2.boundingRect(c)
+    while uglovi != 4:
+        try:
+            mask = cv2.inRange(hsv_image, lowerb=lower_bound, upperb=upper_bound)
+            element = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=kernel_size, anchor=anchor_size)
+            
+            dilatation_dst = cv2.dilate(mask, element)
+            contours, _ = cv2.findContours(dilatation_dst, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            
+            uglovi = len(contours)
+            
+            print(f"treuntno: {uglovi}")
+            showImage(dilatation_dst)
+            konture = contours
+            image = loadImage(vidcap=vidcap)
+            
+        except KeyboardInterrupt:
+            break
 
-# center = (np.round((x+x+w)/2).astype(np.uint16),np.round((y+y+h)/2).astype(np.uint16))
+    return konture
 
-coords = np.float32([[704,711],[821,63],[377,8],[111,548]])
-output = np.float32([[800,0],[0,0],[0,600],[800,600]])
-perspective_transform = cv2.getPerspectiveTransform(coords, output)
 
-# pts = np.float32(np.array([[[800,600]]]))
+def findEdgePoints(contours):
+    edgePoints = []
+    
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
 
-# M = cv2.perspectiveTransform(pts, perspective_transform, )
-# print(M)
+        center = (np.round((x+x+w)/2).astype(np.uint16),np.round((y+y+h)/2).astype(np.uint16))
+        
+        edgePoints.append(center)
+    
+    return edgePoints
 
-# dst = cv2.warpPerspective(image, M, (800,600))
 
-dst = cv2.warpPerspective(image, perspective_transform, (800,600))
-
-cv2.imshow("output", dst)
-cv2.waitKey(0)
+def doPerspectiveTransform(image, edgePoints: tuple):
+    # coords = np.float32([[704,711],[821,63],[377,8],[111,548]])
+    coords = np.float32(edgePoints)
+    
+    # TODO: check the oder in which coords are placed into
+    #       edgePoints and adjust outut accordingly
+    print(edgePoints)
+    output = np.float32([[800,600],[800,0],[0,600],[0,0]])
+    perspective_transform = cv2.getPerspectiveTransform(coords, output)
+    
+    dst = cv2.warpPerspective(image, perspective_transform, (800,600))
+    
+    return dst
 
 if __name__ == "__main__":
-    pass
+    vidcap = setupVidCap()
+    
+    while True:
+        try:
+            img = loadImage(vidcap)
+            
+            contours = getDialatedEdges(vidcap, img, kernel_size=(21,21), anchor_size=(10,10))
+            
+            edges = findEdgePoints(contours=contours)
+
+            shifted = doPerspectiveTransform(img, edgePoints=edges)
+        
+            showImage(shifted)
+        except KeyboardInterrupt:
+            print("exiting...")
+            closeVidCap(vidcap)
